060b428a065d6d6608d6cff638d8de07217c1259
lucasew <lucas59356@gmail.com>
2026-07-16 17:12:38 -0300

    test: punctuation-at-paragraph-end2 wrappers for main languages
    Thin wrappers around the shared PunctuationMarkAtParagraphEnd2 core.

---
+++ internal/languagetool/org/languagetool/rules/es/punctuation_mark_at_paragraph_end2.go
@@ -0,0 +1,19 @@
+package es
+
+import (
+	"github.com/lucasew/lang/internal/languagetool/org/languagetool"
+	"github.com/lucasew/lang/internal/languagetool/org/languagetool/rules"
+)
+
+// PunctuationMarkAtParagraphEnd2 wraps the core rule for this language.
+type PunctuationMarkAtParagraphEnd2 struct {
+	*rules.PunctuationMarkAtParagraphEnd2
+}
+
+func NewPunctuationMarkAtParagraphEnd2(messages map[string]string) *PunctuationMarkAtParagraphEnd2 {
+	return &PunctuationMarkAtParagraphEnd2{PunctuationMarkAtParagraphEnd2: rules.NewPunctuationMarkAtParagraphEnd2(messages)}
+}
+
+func (r *PunctuationMarkAtParagraphEnd2) MatchList(sentences []*languagetool.AnalyzedSentence) []*rules.RuleMatch {
+	return r.PunctuationMarkAtParagraphEnd2.MatchList(sentences)
+}
