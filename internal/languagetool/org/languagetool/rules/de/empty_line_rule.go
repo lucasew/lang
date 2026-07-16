45178b604752a1d53684af3bef19ea6cb93064f7
lucasew <lucas59356@gmail.com>
2026-07-16 17:02:41 -0300

    test: empty-line and multi-whitespace wrappers for main languages
    Thin language packages for DE EN FR ES PT NL IT PL RU around the shared
    EmptyLineRule and MultipleWhitespaceRule cores.

---
+++ internal/languagetool/org/languagetool/rules/de/empty_line_rule.go
@@ -0,0 +1,19 @@
+package de
+
+import (
+	"github.com/lucasew/lang/internal/languagetool/org/languagetool"
+	"github.com/lucasew/lang/internal/languagetool/org/languagetool/rules"
+)
+
+// EmptyLineRule wraps the core EmptyLineRule for this language.
+type EmptyLineRule struct {
+	*rules.EmptyLineRule
+}
+
+func NewEmptyLineRule(messages map[string]string) *EmptyLineRule {
+	return &EmptyLineRule{EmptyLineRule: rules.NewEmptyLineRule(messages)}
+}
+
+func (r *EmptyLineRule) MatchList(sentences []*languagetool.AnalyzedSentence) []*rules.RuleMatch {
+	return r.EmptyLineRule.MatchList(sentences)
+}
