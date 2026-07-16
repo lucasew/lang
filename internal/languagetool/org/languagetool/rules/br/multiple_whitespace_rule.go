61c1ef452c96eb4c7128079e07244c08e32dbd01
lucasew <lucas59356@gmail.com>
2026-07-16 17:04:29 -0300

    test: empty-line and multi-whitespace wrappers for remaining langs
    Extend thin wrappers to AR CA UK SV GA GL DA SK SL RO FA EL BR KM EO ZH
    JA BE SR and other language packages that lacked them.

---
+++ internal/languagetool/org/languagetool/rules/br/multiple_whitespace_rule.go
@@ -0,0 +1,19 @@
+package br
+
+import (
+	"github.com/lucasew/lang/internal/languagetool/org/languagetool"
+	"github.com/lucasew/lang/internal/languagetool/org/languagetool/rules"
+)
+
+// MultipleWhitespaceRule wraps the core MultipleWhitespaceRule for this language.
+type MultipleWhitespaceRule struct {
+	*rules.MultipleWhitespaceRule
+}
+
+func NewMultipleWhitespaceRule(messages map[string]string) *MultipleWhitespaceRule {
+	return &MultipleWhitespaceRule{MultipleWhitespaceRule: rules.NewMultipleWhitespaceRule(messages)}
+}
+
+func (r *MultipleWhitespaceRule) Match(sentences []*languagetool.AnalyzedSentence) []*rules.RuleMatch {
+	return r.MultipleWhitespaceRule.Match(sentences)
+}
